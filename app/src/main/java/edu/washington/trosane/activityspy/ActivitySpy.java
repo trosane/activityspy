4969b1eeecee7c5afbfe4845e13d3f128d7af321
TRENT ROSANE <trosane@uw.edu>
2015-04-14 12:20:06 -0700

    second commit

--- app/src/main/java/edu/washington/trosane/activityspy/ActivitySpy.java
+++ app/src/main/java/edu/washington/trosane/activityspy/ActivitySpy.java
@@ -4,14 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 
 public class ActivitySpy extends ActionBarActivity {
+    public static final String TAG = ActivitySpy.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_spy);
+        Log.i(TAG, "onCreate event fired: ");
     }
 
 
@@ -36,4 +39,43 @@ public class ActivitySpy extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart event fired: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart event fired: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume event fired: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop event fired: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onStop event fired: ");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy event fired: ");
+
+    }
+
+
+
 }
